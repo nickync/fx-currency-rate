42834908ee5255e28775b8178f19b33744b2cceb
nickync <lizheng9024@gmail.com>
2023-06-03 16:11:26 -0400

    web

--- Web/src/App.js
+++ Web/src/App.js
@@ -3,21 +3,21 @@ import CurrencyPanel from './component/CurrencyPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import About from './component/About';
 
 // 1. rolling rate card, 2. info, data source 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Header />
         <Routes>
           {["/", "/home"].map((path, index) => <Route path={path} key={index} element={ <CurrencyPanel />} />)}
           <Route path='about' element={ <About /> } />
         </Routes>
         <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

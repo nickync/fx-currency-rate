9b0f64238320531f3aa0cf568baf5a68ec1c34a2
nickync <lizheng9024@gmail.com>
2023-06-03 16:32:40 -0400

    router

--- Web/src/App.js
+++ Web/src/App.js
@@ -3,23 +3,21 @@ import CurrencyPanel from './component/CurrencyPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header';
 import Footer from './component/Footer';
-import { HashRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './component/About';
 
 // 1. rolling rate card, 2. info, data source 
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
         <Header />
-        
         <Routes>
           {["/", "/home"].map((path, index) => <Route path={path} key={index} element={ <CurrencyPanel />} />)}
-          <Route exact path="/about" element={ <About /> } />
+          <Route path='about' element={ <About /> } />
         </Routes>
-
         <Footer />
-    </Router>
+    </BrowserRouter>
   );
 }
 
--- Web/src/component/Header.jsx
+++ Web/src/component/Header.jsx
@@ -1,16 +1,24 @@
 import { Nav, NavDropdown, Navbar, NavbarBrand } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import {useNavigate} from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const aboutPage = () => {
+    navigate('/about')
+  }
+
+
   return (
     <Navbar className="py-0 fixed-top bg-gradient" variant="dark" bg="dark" expand="lg">
       <Container>
-       <Navbar.Brand className="mx-auto fw-bold text-success" href="home">FX currency finder</Navbar.Brand>
+       <Navbar.Brand className="mx-auto fw-bold text-success" href="/">FX currency finder</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About this site</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link onClick={aboutPage}>About this site</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">

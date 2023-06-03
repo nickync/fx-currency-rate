1dd59617fd68e2a3519907b95a3f8f43e5655f5e
nickync <lizheng9024@gmail.com>
2023-06-03 16:08:53 -0400

    web

--- Web/src/component/Header.jsx
+++ Web/src/component/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="about">About this site</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About this site</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">

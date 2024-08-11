143014c6a779982a12f759586991819f3804de6f
nickync <lizheng9024@gmail.com>
2024-08-11 15:11:00 -0400

    update apit

--- Web/src/api/curencyAPI.js
+++ Web/src/api/curencyAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // export const getRate = async (date) => await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/usd.json`)
 
-export const getRate = async (date) => await axios.get(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/usd.json`)
+export const getRate = async (date) => await axios.get(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${date}/v1/currencies/usd.json`)
 
 
 // export const getDefinition = () => axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json`)
